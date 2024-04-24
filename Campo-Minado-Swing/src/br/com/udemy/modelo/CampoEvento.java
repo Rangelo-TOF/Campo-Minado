2c8169a3c0dbf11ba829e734e5cbaac2b3590cec
Rangelo-TOF <rangelotadeu@outlook.com>
2024-04-24 00:46:03 -0300

    Campo_minado

---
+++ Campo-Minado-Swing/src/br/com/udemy/modelo/CampoEvento.java
@@ -0,0 +1,5 @@
+package br.com.udemy.modelo;
+
+public enum CampoEvento {
+		ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
+}
---
+++ Campo-Minado-Swing/src/br/com/udemy/modelo/CampoObservador.java
@@ -0,0 +1,6 @@
+package br.com.udemy.modelo;
+
+public interface CampoObservador {
+	public void eventoOcorreu (Campo campo, CampoEvento evento);
+
+}
